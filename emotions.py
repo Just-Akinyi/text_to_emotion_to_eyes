c9c087e49a2c167e7da9e15f5c276222902646c0
Justin <justakinyi@gmail.com>
2022-11-24 21:33:01 +0300

    finally

--- emotions.py
+++ emotions.py
@@ -20,17 +20,26 @@ for sentence in sentences_list:
     # output is emotional dicts for each sentence
     emotion = te.get_emotion(sentence)
     emotional_list.append(emotion)
-    print(emotional_list)
+print(emotional_list)
 print('-------------------------------------------------')
 # so far we have two lists 1 for sentences another for their emotions
 pure_emotions = []
-for states in emotional_list:
-    for emotional_value in states.values():
-        # check if the emotion has bonus and keep its name
-        if round(emotional_value) > 0:  # what if they tie
-            pure_emotions.append(states.keys())
-            print(pure_emotions)
-
+for i in range(len(pure_emotions)):
+    for key, val in emotional_list[i].items():
+        if val > 1:
+            pure_emotions.append(key)
+        else:
+            key[i] = "neutral"
+            pure_emotions.append(key)
+        print(pure_emotions)
+    # print (pure_emotions)
+# for states in emotional_list:
+#     for emotional_value in states.values():
+#         # check if the emotion has bonus and keep its name
+#         if round(emotional_value) > 0:  # what if they tie
+#             pure_emotions.append(states.keys())
+# print(pure_emotions)
+ 
 # lets show emotions on face now ['sad', 'happy', 'angry']
 #the library supports 5 emotions
 for mood in pure_emotions:
